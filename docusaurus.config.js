ced815b9352e64f3965828f8bf79fc67fafa296d
Akil Burgess <64106580+akilburge@users.noreply.github.com>
2022-01-31 22:40:32 -0500

    Hook up sitemap plugin (#1)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,6 +85,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+        },
       }),
     ],
   ],
